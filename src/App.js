62875613d30a64cdd4d89c5f47d74832054970f4
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2019-10-09 12:22:30 +0530

    Implimentted the nav bar with custom css to the hearder of the homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import Navbar from './components/layout/NavBar';
+import UserItem from './components/users/UserItem';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to GitHub Finder</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <UserItem />
+      </div>
+    );
+  }
 }
 
 export default App;
