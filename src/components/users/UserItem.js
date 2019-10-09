cb12a159889c833c2fd50c59cb282a7ccb0c7473
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2019-10-09 14:26:34 +0530

    Getting the user from github api insted of hardcoded users and axios package is used for fetching data

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,35 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  //   constructor() {
-  //     super();
-  //     // state is the just object of the javascript
-  //     this.state = {
-  //       id: 'id',
-  //       login: 'mojombo',
-  //       avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-  //       html_url: 'https://github.com/mojombo'
-  //     };
-  //   }
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className='car text-center'>
-        <img
-          src={avatar_url}
-          alt='use_Avatar'
-          className='round-img'
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='car text-center'>
+      <img
+        src={avatar_url}
+        alt='use_Avatar'
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.prototype = {
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
