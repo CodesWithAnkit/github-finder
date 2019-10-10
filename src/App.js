95ca8fdd3bef1754281cd34573eba1a0c08c09d5
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2019-10-10 13:29:58 +0530

    Stablishing the alert state and its component for when the user will search a empty search bar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/NavBar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   // async componentDidMount() {
@@ -30,16 +32,26 @@ class App extends Component {
 
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
     const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
